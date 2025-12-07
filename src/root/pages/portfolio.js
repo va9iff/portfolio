d47cee851f968846a7b5acd3fdbd481b48d70ccc
va9iff <va9iff@gmail.com>
2025-12-07 19:54:43 +0400

    start to make portfolio entry styles

--- src/root/pages/portfolio.js
+++ src/root/pages/portfolio.js
@@ -1,16 +1,77 @@
 // @ts-check
 
-import { tags, set } from "../hu.js"
-const { div, h3 } = tags
+import { cards } from "../cards.js"
+import { tags, style, set } from "../hu.js"
+const { div, h1, h3, span } = tags
+
+const row = style({ display: 'flex' })
+const col = style({ display: 'flex', flexDirection: 'column' })
 
 export const portfolio = div(
-    set({
-        className: 'g'
-    }),
-    h3('project 1'),
-    h3('project 2'),
-    h3('project 3'),
-    h3('project 4'),
+    h1("Portfolio", 
+        set({
+            className: 'title'
+        })
+    ),
+    div(
+        set({
+            className: "g"
+        }),
+        style({
+            background: '#161818',
+            borderRadius: '17px',
+            flexDirection: 'columnt',
+            padding: '23px',
+        }),
+        div(
+            style({ 
+                float: 'left', 
+                marginRight: '10px',
+                marginBottom: '4px',
+            }),
+            cards({
+                bg: "#161818",
+                width: 400 + 'px',
+                aspect: 800 / 600,
+                slides: [
+                    {
+                        src: 'assets/glitchy.png',
+                    },
+                    {
+                        src: 'assets/blurry.jpg',
+                    },
+                    {
+                        src: 'assets/glitchy.png',
+                    },
+                    {
+                        src: 'assets/glitchy.png',
+                    },
+                ]
+            }),
+        ),
+        h3(
+            "TEKNOFEST 2025: Istanbul",
+            style({
+                fontSize: '45px',
+                fontWeight: '600',
+                paddingBottom: '4px',
+                paddingTop: '6px',
+                paddingLeft: '10px',
+            })
+        ),
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+        'sdlkfjal lkjsfdal; hjsdiofaj klasdfj klsadf ;kls ja ;klfj sda;ljhasf lkj afdlk ',
+    ),
 )
 
 
